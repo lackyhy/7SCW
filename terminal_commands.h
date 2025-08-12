d5748ada942686df442e02b712d3370bba43426c
ラッキーハイ <105211599+lackyhy@users.noreply.github.com>
2025-08-12 14:05:58 +0300

    Update terminal_commands.h

--- terminal_commands.h
+++ terminal_commands.h
@@ -27,6 +27,14 @@ void showHashHelp();
 string generateRandomHash(int length);
 void saveHashesToFile(const vector<string>& hashes, const string& filename);
 
+// Network functions
+void showNetworkMenu();
+void testNetworkConnection();
+void extendedPingTest();
+void showSavedNetworks();
+void showAvailableNetworks();
+void pingSite(const string& url);
+
 // Command parsing and execution
 bool executeCommand(const string& command, const vector<string>& args);
 
